410672e2fbf135c4eef4dc7a600867296fefd32d
williambl <william@williambl.com>
2021-06-07 20:47:03 +0100

    Add auto-setting loot table for diamond chest

--- src/main/java/com/williambl/diamondchest/DiamondChestBlock.java
+++ src/main/java/com/williambl/diamondchest/DiamondChestBlock.java
@@ -3,11 +3,15 @@ package com.williambl.diamondchest;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ChestBlock;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.IBlockReader;
 
 import javax.annotation.Nullable;
+import java.util.Random;
 
 public class DiamondChestBlock extends ChestBlock {
+    private static Random random = new Random();
+
     public DiamondChestBlock(Properties properties) {
         super(properties, DiamondChest.DIAMOND_CHEST_BLOCK_ENTITY_TYPE::get);
     }
@@ -15,6 +19,8 @@ public class DiamondChestBlock extends ChestBlock {
     @Nullable
     @Override
     public TileEntity createTileEntity(BlockState state, IBlockReader world) {
-        return new DiamondChestBlockEntity();
+        DiamondChestBlockEntity blockEntity = new DiamondChestBlockEntity();
+        blockEntity.setLootTable(new ResourceLocation("diamond_chest:chests/diamond_chest"), random.nextInt());
+        return blockEntity;
     }
 }
